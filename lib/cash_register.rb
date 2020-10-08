02b8cede991b3404d2588d5c846467db549c1d3b
JakesProjects1 <jacob@kagon.net>
2020-10-08 13:02:16 -0400

    new changes

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require 'pry'
+class CashRegister
+    
+    attr_reader 
+    attr_accessor :discount, :total, :title, :price, :quantity
+    
+    def initialize(discount=20)
+        @total = 0
+        @discount = discount
+    end
+
+    
+    def add_item(title, price, quantity = 1)
+        self.total += price * quantity
+    end
+    #binding.pry
+
+    def apply_discount
+        @discount = 200
+       new_total = self.total *= @discount
+    end
+
+
+end
 
